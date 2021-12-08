bfc77466b0e2a5b2e0796a7151dc8f00768e6f08
Shawn Kebel <fxrender@outlook.com>
2021-12-08 00:09:01 -0600

    :wrench: config.encryption

--- app/config.js
+++ app/config.js
@@ -8,5 +8,12 @@ export default {
     name: "gradeBookDB",
     uri: process.env.MONGODB_URI,
   },
+
+  // Add encryption object
+  encryption: {
+    expiresIn: process.env.EXPIRES_IN || "7d", // How long till expires
+    saltRounds: process.env.SALT_ROUNDS || 10, // adds 10 random characters
+    secret: process.env.ENCRYPTION_SECRET || "secret", // ... or "secret"
+  },
   port: process.env.PORT || 3000,
 };
