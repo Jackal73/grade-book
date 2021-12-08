6feeec4f1aa1d4ad21cb035092778397f1e1c491
Shawn Kebel <fxrender@outlook.com>
2021-12-08 00:24:40 -0600

    :bulb: routes/index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,10 +4,12 @@ import studentsRouter from "./students.js";
 
 const router = new Router();
 
+// Localhost "Hello World" : test
 router.get("/", (_, res) => {
   res.send("Hello World!");
 });
 
+// Middleware for admin and student routes
 router.use("/admin", adminRouter);
 router.use("/students", studentsRouter);
 
