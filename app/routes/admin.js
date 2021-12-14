59b1d1ca3a26a3846a57aa84ce9f86949cfa08bf
Shawn Kebel <fxrender@outlook.com>
2021-12-13 22:16:40 -0600

    :sparkles: Validate Admin

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import adminController from "../controllers/admin.js";
+import Admin from "../models/Users/Admin.js";
 
 const router = new Router();
 
@@ -10,12 +11,21 @@ router.get("/", (_, res) => {
 // Admin route
 router.post("/register", async (req, res) => {
   try {
-    const { username, password } = req.body;
+    const admin = new Admin(req.body);
 
-    await adminController.create(username, password);
+    console.log(admin);
+
+    const errors = admin.validate();
+
+    if (errors.length) {
+      throw new Error(errors.join("\n"));
+    }
+
+    await adminController.create(admin);
 
     // Log in the user and wait for Json Web Token
     const token = await adminController.show(username, password);
+
     res.status(201).send(token);
   } catch ({ message }) {
     res.status(400).json({ message });
